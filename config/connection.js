63ee73849400ba4b0cbec152c8adbf23cefea2be
clshu <ccss0630@yahoo.com>
2016-11-08 01:03:19 -0600

    Add process.env.JAWSDB_URL to connection and change PORT

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,7 @@
 Here is where you make the connection to the database and export and used by the O.R.M.
 */
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-	port: 3306,
-	host: 'ehc1u4pmphj917qf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-	user: 'apfn96qh4rrmnuh0',
-	password: 'zwou44c8mchdhl07',
-	database: 'ddlrb2kn7cuc1wuj'
-});
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 connection.connect(function (err) {
 	if (err) {
--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.set('view engine', 'handlebars');
 var routes = require('./controllers/burgers_controller.js');
 app.use('/', routes);
 
-var PORT = process.env.JAWSDB_URL || 3000;
+var PORT = process.env.PORT || 3000;
 app.listen(PORT, function() {
 	console.log('App listening on PORT ' + PORT);
 });
