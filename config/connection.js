d3f10f1c6e9ae606eb4cd14f8d6106b32255fc42
clshu <ccss0630@yahoo.com>
2016-11-08 01:29:01 -0600

    Make creating connection work for localhost and Heroku

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,9 @@
 Here is where you make the connection to the database and export and used by the O.R.M.
 */
 var mysql = require('mysql');
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
+var localConnection = "mysql://root:puppetmaster@localhost:3306/burgers_db";
+var dbConnection = process.env.JAWSDB_URL || localConnection;
+var connection = mysql.createConnection(dbConnection);
 
 connection.connect(function (err) {
 	if (err) {
